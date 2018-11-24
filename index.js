27cdc26edc729b4d1d4a7a696717351f6708d7b1
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-11-24 18:31:12 +0530

    feat(index.js): add xref API

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const port = parseInt(process.env.PORT, 10) || 3000;
 const app = require("express")();
 const bodyParser = require("body-parser");
+const xrefResponseBody = require("respec-xref-route");
+const xrefData = require("./xref-data.json");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.post("/xref", (req, res) => {
+  const response = xrefResponseBody(req.body, xrefData);
+  res.json(response);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
