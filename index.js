effe739869fad4d1c56478b410527c80632a57b8
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-11-26 15:20:37 +0530

    fix: .route suffix in import

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 const app = require("express")();
 const bodyParser = require("body-parser");
 
-app.post("/xref", bodyParser.json(), require("./routes/xref/"));
+app.post("/xref", bodyParser.json(), require("./routes/xref/").route);
 app.post(
   "/xref/update",
-  bodyParser.json({ verify: rawBodyParser }),
-  require("./routes/xref/update")
+  bodyParser.json({
+    verify: rawBodyParser
+  }),
+  require("./routes/xref/update").route
 );
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
