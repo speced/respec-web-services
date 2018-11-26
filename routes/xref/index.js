719101b073de8d7d42e06c3dc9ebfc727ce3a2d7
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-11-26 16:07:16 +0530

    watch for changes and xref-data.json and reload data

--- routes/xref/index.js
+++ routes/xref/index.js
@@ -1,7 +1,19 @@
 const xrefResponseBody = require("respec-xref-route");
-const xrefData = require("../../xref-data.json");
+const { watch, readFileSync } = require("fs");
+const path = require("path");
+
+const dataFile = path.resolve(path.join(process.cwd(), "xref-data.json"));
+let xrefData = JSON.parse(readFileSync(dataFile, "utf8"));
 
 module.exports.route = function route(req, res) {
   const body = xrefResponseBody(req.body, xrefData);
   res.json(body);
 };
+
+// reload xref data if ref-data.json is modified
+watch(dataFile, eventType => {
+  if (eventType === "change") {
+    console.log("xref-data.json modified");
+    xrefData = JSON.parse(readFileSync(dataFile, "utf8"));
+  }
+});
