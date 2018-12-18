5dbf2f3a7faf6e4bde9a977e5ea15618dcb79aa1
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-12-18 15:39:01 +0530

    fix: debounce reloadFile to prevent parsing incomplete json

--- routes/xref/index.js
+++ routes/xref/index.js
@@ -11,9 +11,22 @@ module.exports.route = function route(req, res) {
 };
 
 // reload xref data if xref-data.json is modified
-watch(dataFile, eventType => {
+watch(dataFile, debounce(reloadFile, 1000));
+
+function reloadFile(eventType) {
   if (eventType === "change") {
-    console.log("xref-data.json modified");
+    console.log("xref-data.json modified. reloading...");
     xrefData = JSON.parse(readFileSync(dataFile, "utf8"));
   }
-});
+}
+
+function debounce(fn, delay) {
+  let timer;
+  return (...args) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+      timer = null;
+    }, delay);
+  };
+}
