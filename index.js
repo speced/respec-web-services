b0d4dc79fa0380c6ae33def073494a7612607978
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-12-22 18:20:58 +0530

    feat(/xref): enable cors

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const port = parseInt(process.env.PORT, 10) || 3000;
 const app = require("express")();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.post("/xref", bodyParser.json(), require("./routes/xref/").route);
+// for preflight request
+app.options("/xref", cors({ methods: ["POST"] }));
+app.post(
+  "/xref",
+  bodyParser.json(),
+  cors(),
+  require("./routes/xref/").route
+);
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
