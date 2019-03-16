183434802c5a3b17d97a0a13b5f4e2ab0f05bce5
Marcos Cáceres <marcos@marcosc.com>
2019-03-16 22:16:13 +1100

    feat: gzip all the things (#9)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 const app = require("express")();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const compression = require("compression");
+app.use(compression());
 const rawBodyParser = require("./utils/raw-body-parser");
 
 // for preflight request
@@ -11,7 +13,7 @@ app.post("/xref", bodyParser.json(), cors(), require("./routes/xref/").route);
 app.post(
   "/xref/update",
   bodyParser.json({
-    verify: rawBodyParser
+    verify: rawBodyParser,
   }),
   require("./routes/xref/update").route
 );
