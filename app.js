388db8475f7229ce42b07e2a2115ad7b00007c04
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-04-27 21:42:41 +0530

    feat(xref/meta): add route to expose metadata

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ app.use(
 );
 
 // for preflight request
-app.options("/xref", cors({ methods: ["POST"] }));
+app.options("/xref", cors({ methods: ["POST", "GET"] }));
 app.post("/xref", bodyParser.json(), cors(), require("./routes/xref/").route);
+app.get("/xref/meta", cors(), require("./routes/xref/meta").route);
 app.post(
   "/xref/update",
   bodyParser.json({ verify: rawBodyParser }),
