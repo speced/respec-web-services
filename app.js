9e6135fd1f24217343a1c48efcdfd8c2627d8403
Marcos Cáceres <mcaceres@mozilla.com>
2019-05-06 18:44:51 +1000

    sec: add halmet to control security aspects (#27)

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ const compression = require("compression");
 app.use(compression());
 const rawBodyParser = require("./utils/raw-body-parser");
 const morgan = require("morgan");
+const helmet = require("helmet");
 
 // loggin
 app.enable("trust proxy"); // for :remote-addr
 app.use(
-  morgan(":date[iso] | :remote-addr | :method :status :url | :referrer | :res[content-length] | :response-time ms")
+  morgan(
+    ":date[iso] | :remote-addr | :method :status :url | :referrer | :res[content-length] | :response-time ms"
+  )
 );
+
+// Security
+// Defaults https://www.npmjs.com/package/helmet#how-it-works
+app.use(helmet({
+  frameguard: false, // Allow for UI inclusion as iframe in ReSpec pill.
+}));
 
 // for preflight request
 app.options("/xref", cors({ methods: ["POST", "GET"] }));
