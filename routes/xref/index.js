af763b690e034f648e820d7f1e1175ec85cdd249
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-08-19 15:38:19 +0530

    feat(xref): log error counts

--- routes/xref/index.js
+++ routes/xref/index.js
@@ -2,7 +2,24 @@
 const { search } = require("respec-xref-route/search");
 
 module.exports.route = function route(req, res) {
-  const { keys, options } = req.body;
+  const { keys = [], options } = req.body;
   const body = search(keys, options);
+
+  const errors = getErrorCount(body.result);
+  if (errors / keys.length > 0.05) {
+    // add to logs if error rate is more than 5%
+    Object.assign(res.locals, { errors, queries: keys.length });
+  }
+
   res.json(body);
 };
+
+function getErrorCount(results) {
+  let errorCount = 0;
+  for (const [, entries] of results) {
+    if (entries.length !== 1) {
+      errorCount++;
+    }
+  }
+  return errorCount;
+}
