b83ddaf924e8a3f02f17c7c6e0c2530d2421891c
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-08-19 18:20:04 +0530

    feat(logging): reduce noise in logs

--- utils/logging.js
+++ utils/logging.js
@@ -10,4 +10,21 @@ morgan.token("locals", (req, res) => {
 const format =
   ":date[iso] | :remote-addr | :method :status :url | :referrer | :res[content-length] | :response-time ms | :locals";
 
-module.exports = () => morgan(format);
+/** @type {import('morgan').Options} */
+const options = {
+  skip(req, res) {
+    const { method, path, hostname } = req;
+    const { statusCode } = res;
+    switch (true) {
+      // /xref pre-flight request
+      case method === "OPTIONS" && path === "/xref" && statusCode === 204:
+      // automated tests
+      case hostname === "localhost:9876":
+        return true;
+      default:
+        return false;
+    }
+  },
+};
+
+module.exports = () => morgan(format, options);
