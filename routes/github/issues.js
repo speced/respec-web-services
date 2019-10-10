52550d7608e2b5e81cddb3c35945367bdff67b68
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-10-10 14:40:02 +0530

    unique issues only

--- routes/github/issues.js
+++ routes/github/issues.js
@@ -14,12 +14,16 @@ module.exports.route = async function route(req, res) {
   }
   if (Array.isArray(req.query.issues)) {
     // ?issues=879,817&issues=912 => ?issues=879,817,912
-    req.query.issues = req.query.issues.join(",")
+    req.query.issues = req.query.issues.join(",");
   }
-  const issues = req.query.issues
-    .split(/\,/)
-    .map(issue => parseInt(issue.trim(), 10))
-    .filter(issue => !Number.isNaN(issue) && issue > 0);
+  const issues = [
+    ...new Set(
+      /** @type {string} */ (req.query.issues)
+        .split(/\,/)
+        .map(issue => parseInt(issue.trim(), 10))
+        .filter(issue => !Number.isNaN(issue) && issue > 0),
+    ),
+  ];
 
   // cache all results for 30 min (1800 seconds)
   res.set("Cache-Control", "max-age=1800");
@@ -29,6 +33,6 @@ module.exports.route = async function route(req, res) {
     if (result === null) return res.sendStatus(404);
     res.json(result);
   } catch (error) {
-    res.sendStatus(500)
+    res.sendStatus(500);
   }
 };
