6449a732af7ba7e7ceeb143872a871ab0559a61b
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-05-26 13:14:51 +0530

    feat(dashboard): add respec-w3c size tracker (#74)

---
+++ static/dashboard/size-chart.js
@@ -0,0 +1,81 @@
+google.charts.load("current", { packages: ["line", "corechart"] });
+google.charts.setOnLoadCallback(main);
+
+const form = document.getElementById("size-form");
+const columnMap = { time: 0, sha: 1, size: 2, gzipSize: 3 };
+
+async function main() {
+  const entries = await fetchData();
+  const dataTable = getDataTable(entries);
+  window.addEventListener("resize", () => drawChart(dataTable));
+  form.addEventListener("change", () => drawChart(dataTable));
+  drawChart(dataTable);
+}
+
+async function fetchData() {
+  const text = await fetch("/respec/size").then(res => res.text());
+  const lines = text.trimEnd().split("\n");
+  return lines.map(entry =>
+    JSON.parse(entry, (k, v) => (k === "time" ? new Date(v * 1000) : v)),
+  );
+}
+
+function getDataTable(entries) {
+  const table = new google.visualization.DataTable();
+  table.addColumn("datetime", "Time");
+  table.addColumn("string", "Commit SHA");
+  table.addColumn("number", "Size (bytes)");
+  table.addColumn("number", "Size (bytes)");
+
+  const idxMap = Object.entries(columnMap).sort((a, b) => a[1] - b[1]);
+  const toRow = entry => idxMap.map(a => entry[a[0]]);
+  table.addRows(entries.map(toRow));
+
+  return table;
+}
+
+/** @param {google.visualization.DataTable} data */
+function drawChart(data) {
+  const { checked: showGzipSize } = form.gzipSize;
+  const months = parseInt(form.duration.value, 10);
+
+  let viewWindow;
+  if (months) {
+    const date = new Date();
+    date.setMonth(date.getMonth() - months);
+    viewWindow = { min: date };
+  }
+
+  const title = showGzipSize ? "Size (gzip, bytes)" : "Size (bytes)";
+  /** @type {google.visualization.LineChartOptions} */
+  const drawOptions = {
+    theme: "maximized",
+    legend: "none",
+    tooltip: { trigger: "selection" },
+    vAxis: { title, format: "short" },
+    hAxis: { viewWindow },
+    explorer: {
+      keepInBounds: true,
+      axis: "horizontal",
+      actions: ["dragToZoom", "rightClickToReset"],
+      maxZoomIn: 0.1,
+    },
+  };
+
+  const elem = document.getElementById("size-chart");
+  const chart = new google.visualization.LineChart(elem);
+  chart.setAction({
+    id: "view_commit",
+    text: "View commit ↗️",
+    action() {
+      const { row } = chart.getSelection().slice(-1)[0];
+      const sha = data.getValue(row, 1);
+      window.open(`https://github.com/w3c/respec/commit/${sha}`);
+    },
+  });
+
+  const view = new google.visualization.DataView(data);
+  const key = showGzipSize ? "gzipSize" : "size";
+  view.setColumns([columnMap.time, columnMap[key]]);
+  chart.draw(view, drawOptions);
+}
