ef7ba090e84eef9482139c77169e0d0830ad6291
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-07-15 15:40:42 +0530

    chore: temporarily redirect home page to /docs

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use("/github/:org/:repo", require("./routes/github/").routes);
 app.use("/respec", require("./routes/respec/").routes);
 app.use("/w3c", require("./routes/w3c/").routes);
 app.use("/docs", require("./routes/docs/").routes);
+app.get("/", (req, res) => res.redirect("/docs/"));
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.listen(port, () => console.log(`Listening on port ${port}!`));
