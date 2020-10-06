092a1942113f34834803495d8e411860ec818f50
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-10-06 12:57:38 +0530

    set to 1 day instead

--- routes/w3c/group.js
+++ routes/w3c/group.js
@@ -23,7 +23,7 @@ const API_KEY = env("W3C_API_KEY");
  * @property {"PP2017" | "PP2020" | null} [Group.patentPolicy]
  */
 /** @type {MemCache<Group>} */
-const cache = new MemCache(ms("3 days"));
+const cache = new MemCache(ms("1 day"));
 
 // Support non W3C shortnames for backward compatibility.
 const LEGACY_SHORTNAMES = new Map([
