c2322ab5a213c4bf214b2249f0661ccbbbd581b5
Sid Vishnoi <sidvishnoi8@gmail.com>
2021-02-16 15:26:26 +0530

    refactor: start typescript migration

--- utils/dotenv.js
+++ utils/dotenv.js
@@ -1,9 +1,19 @@
 // https://github.com/motdotla/dotenv/issues/133#issuecomment-255298822
 
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
+import { join } from "path";
+import { readdirSync } from "fs";
 
 import dotenv from "dotenv";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: join(__dirname, "..", ".env") });
+import { legacyDirname } from "./misc.js";
+const __dirname = legacyDirname(import.meta);
+
+// Traverse to parent directories until a `.env` file is found.
+// See: https://github.com/motdotla/dotenv/issues/238#issuecomment-348017037
+let currentDir = __dirname;
+while (!readdirSync(currentDir).includes(".env")) {
+  const newCurrentDir = join(currentDir, "..");
+  if (currentDir === newCurrentDir) break;
+  currentDir = newCurrentDir;
+}
+dotenv.config({ path: join(currentDir, ".env") });
