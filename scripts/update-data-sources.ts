d187476d6b25299f869b12d08ad2798c7e3de5db
Marcos Cáceres <marcos@marcosc.com>
2021-04-30 18:35:32 +1000

    fix(scripts/update-data-sources): ensure data dir exists (#188)
    Co-authored-by: Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>

--- scripts/update-data-sources.ts
+++ scripts/update-data-sources.ts
@@ -1,9 +1,14 @@
 import "../utils/dotenv.js";
+import { mkdir } from "fs/promises";
+import { env } from "../utils/misc.js";
 import caniuse from "../routes/caniuse/lib/scraper.js";
 import xref from "../routes/xref/lib/scraper.js";
 import w3cGroupsList from "./update-w3c-groups-list.js";
 
 async function update() {
+  // ensure the data directory exists
+  await mkdir(env("DATA_DIR"), { recursive: true });
+
   console.group("caniuse");
   await caniuse({ forceUpdate: true });
   console.groupEnd();
