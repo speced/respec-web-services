f49db87887c98ebd03fdfa88d7509591ab11c159
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-05-26 15:27:34 +0530

    chore(deps): update dependencies

--- utils/dotenv.ts
+++
@@ -1,7 +0,0 @@
-// https://github.com/motdotla/dotenv/issues/133#issuecomment-255298822
-
-import { join } from "path";
-import dotenv from "dotenv";
-import { PROJECT_ROOT } from "./constants.js";
-
-dotenv.config({ path: join(PROJECT_ROOT, ".env") });
