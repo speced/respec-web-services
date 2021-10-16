8fbc654471cf232daadd93fcc7d2afdee9870181
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-10-16 18:31:23 +0530

    refactor(scripts/*): use top-level await (#239)

--- scripts/update-data-sources.ts
+++ scripts/update-data-sources.ts
@@ -5,24 +5,17 @@ import caniuse from "../routes/caniuse/lib/scraper.js";
 import xref from "../routes/xref/lib/scraper.js";
 import w3cGroupsList from "./update-w3c-groups-list.js";
 
-async function update() {
-  // ensure the data directory exists
-  await mkdir(env("DATA_DIR"), { recursive: true });
+// ensure the data directory exists
+await mkdir(env("DATA_DIR"), { recursive: true });
 
-  console.group("caniuse");
-  await caniuse({ forceUpdate: true });
-  console.groupEnd();
+console.group("caniuse");
+await caniuse({ forceUpdate: true });
+console.groupEnd();
 
-  console.group("xref");
-  await xref({ forceUpdate: true });
-  console.groupEnd();
+console.group("xref");
+await xref({ forceUpdate: true });
+console.groupEnd();
 
-  console.group("W3C Groups List");
-  await w3cGroupsList();
-  console.groupEnd();
-}
-
-update().catch(error => {
-  console.log(error);
-  process.exit(1);
-});
+console.group("W3C Groups List");
+await w3cGroupsList();
+console.groupEnd();
