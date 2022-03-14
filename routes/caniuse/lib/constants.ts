d16754d6c470315f460f80835c00b680612c9f30
Marcos Cáceres <marcos@marcosc.com>
2022-03-14 17:20:44 +1100

    style(caniuse/constants): use double quotes

--- routes/caniuse/lib/constants.ts
+++ routes/caniuse/lib/constants.ts
@@ -1,32 +1,32 @@
 export const BROWSERS = new Map([
-  ['and_chr', 'Chrome (Android)'],
-  ['and_ff', 'Firefox (Android)'],
-  ['and_uc', 'UC Browser (Android)'],
-  ['android', 'Android'],
-  ['bb', 'Blackberry'],
-  ['chrome', 'Chrome'],
-  ['edge', 'Edge'],
-  ['firefox', 'Firefox'],
-  ['ie', 'IE'],
-  ['ios_saf', 'Safari (iOS)'],
-  ['op_mini', 'Opera Mini'],
-  ['op_mob', 'Opera Mobile'],
-  ['opera', 'Opera'],
-  ['safari', 'Safari'],
-  ['samsung', 'Samsung Internet'],
+  ["and_chr", "Chrome (Android)"],
+  ["and_ff", "Firefox (Android)"],
+  ["and_uc", "UC Browser (Android)"],
+  ["android", "Android"],
+  ["bb", "Blackberry"],
+  ["chrome", "Chrome"],
+  ["edge", "Edge"],
+  ["firefox", "Firefox"],
+  ["ie", "IE"],
+  ["ios_saf", "Safari (iOS)"],
+  ["op_mini", "Opera Mini"],
+  ["op_mob", "Opera Mobile"],
+  ["opera", "Opera"],
+  ["safari", "Safari"],
+  ["samsung", "Samsung Internet"],
 ]);
 
 export const DEFAULT_BROWSERS = ["chrome", "firefox", "safari", "edge"];
 
 // Keys from https://github.com/Fyrd/caniuse/blob/master/CONTRIBUTING.md
 export const SUPPORT_TITLES = new Map([
-  ['y', 'Supported.'],
-  ['a', 'Almost supported (aka Partial support).'],
-  ['n', 'No support, or disabled by default.'],
-  ['p', 'No support, but has Polyfill.'],
-  ['u', 'Support unknown.'],
-  ['x', 'Requires prefix to work.'],
-  ['d', 'Disabled by default (needs to enabled).'],
+  ["y", "Supported."],
+  ["a", "Almost supported (aka Partial support)."],
+  ["n", "No support, or disabled by default."],
+  ["p", "No support, but has Polyfill."],
+  ["u", "Support unknown."],
+  ["x", "Requires prefix to work."],
+  ["d", "Disabled by default (needs to enabled)."],
 ]);
 
 export type SupportKeys = ("y" | "n" | "a" | string)[];
