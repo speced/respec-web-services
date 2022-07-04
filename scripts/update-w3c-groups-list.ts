speced/respec-web-services

commit b1af3ee86670a8fa1ca10888535910c89ddffacc

fix(w3c/groups): include missing TF member groups (#316)

scripts/update-w3c-groups-list.ts
@@ -24,15 +24,25 @@ const mapGroupType = new Map([
   ["_miscellaneous_", "other"],
 ]);
 
-interface GroupResponse {
-  type: string;
-  shortname: string;
+interface GroupBase {
   id: number;
+  shortname: string;
   name: string;
+  discr: string;
+  is_closed: boolean;
   _links: {
     homepage?: { href: string };
   };
 }
+interface GroupResponseWithoutMembers extends GroupBase {
+  discr: "w3cgroup";
+  type: string;
+}
+interface GroupResponseWithMembers extends GroupBase {
+  discr: "tf";
+  members: GroupResponseWithoutMembers[];
+}
+type GroupResponse = GroupResponseWithoutMembers | GroupResponseWithMembers;
 
 interface APIResponse {
   _embedded: {
@@ -69,8 +79,11 @@ export default async function update() {
     r => r.json() as Promise<APIResponse>,
   );
 
-  console.log("Processing results...");
-  for (const group of json._embedded.groups) {
+  const groups = json._embedded.groups.flatMap(g =>
+    g.discr === "w3cgroup" ? g : g.discr === "tf" ? g.members : [],
+  );
+  console.log(`Processing ${groups.length} items...`);
+  for (const group of groups) {
     const type = mapGroupType.get(group.type);
     if (!type) continue;
 
@@ -91,7 +104,7 @@ export default async function update() {
     (group: GroupResponse) => group.shortname === "ab",
   ];
   for (const filter of otherGroupFilters) {
-    const group = json._embedded.groups.find(filter);
+    const group = groups.find(filter);
     if (!group) continue;
     const { shortname, id, name, _links: links } = group;
     const url = links.homepage?.href;

commit dec2e04d71ea9da5b9888ae41a079c922e7b3760

fix(w3c/groups): `discr` can be `"group"` too (for tag, ab)
Regression from b1af3ee86670a8fa1ca10888535910c89ddffacc

scripts/update-w3c-groups-list.ts
@@ -35,7 +35,7 @@ interface GroupBase {
   };
 }
 interface GroupResponseWithoutMembers extends GroupBase {
-  discr: "w3cgroup";
+  discr: "w3cgroup" | "group";
   type: string;
 }
 interface GroupResponseWithMembers extends GroupBase {
@@ -79,9 +79,17 @@ export default async function update() {
     r => r.json() as Promise<APIResponse>,
   );
 
-  const groups = json._embedded.groups.flatMap(g =>
-    g.discr === "w3cgroup" ? g : g.discr === "tf" ? g.members : [],
-  );
+  const groups = json._embedded.groups.flatMap(g => {
+    switch (g.discr) {
+      case "w3cgroup":
+      case "group":
+        return g;
+      case "tf":
+        return g.members;
+      default:
+        return [];
+    }
+  });
   console.log(`Processing ${groups.length} items...`);
   for (const group of groups) {
     const type = mapGroupType.get(group.type);