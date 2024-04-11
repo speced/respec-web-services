f2dbd806dc1a898dfc7a1f5b41865d414ffa091d
Marcos Cáceres <marcos@marcosc.com>
2024-04-12 09:48:14 +1000

    fix: sanitize < and > in spec names
    For specs like "The <model> element", which otherwise end up as actual HTML tags 😬

--- static/xref/script.js
+++ static/xref/script.js
@@ -148,7 +148,9 @@ function renderResults(entries, query) {
   for (const entry of entries) {
     const specInfo = metadata.specs[entry.status][entry.spec];
     const link = new URL(entry.uri, specInfo.url).href;
-    const title = specInfo.title;
+    const title = specInfo.title.replaceAll(/[<>]/g, match => {
+      return match === '<' ? '&lt;' : '&gt;';
+    });
     const cite = metadata.types.idl.has(entry.type)
       ? howToCiteIDL(term, entry)
       : metadata.types.markup.has(entry.type)
