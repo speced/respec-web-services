cf4fab5a03fdd5d9cc8f5f18e819831ebb8b01ce
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-08-23 13:55:45 +0530

    nits

--- routes/respec/builds/update.ts
+++ routes/respec/builds/update.ts
@@ -10,15 +10,14 @@ export const PKG_DIR = path.join(env("DATA_DIR"), "respec", "package");
 
 export default async function route(req: Request, res: Response) {
   res.type("text/plain");
-  if (typeof req.body.action !== "string") {
+  const action = req.body.action;
+  if (typeof action !== "string") {
     return res.status(400).send("Missing 'action' in body");
   }
-  if (req.body.action !== "released") {
+  if (action !== "released") {
     res.status(400); // Bad request
     res.locals.reason = `action-not-released`;
-    const msg = `Webhook payload was for ${JSON.stringify(
-      req.body.action,
-    )}, ignored.`;
+    const msg = `Webhook payload was for ${JSON.stringify(action)}, ignored.`;
     return res.send(msg);
   }
 
