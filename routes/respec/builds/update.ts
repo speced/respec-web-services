a6e6e11caa263115b69cc7c3a0afc7bb1ddd14dc
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-08-23 13:39:43 +0530

    feat(/respec/builds): host latest release from npm

---
+++ routes/respec/builds/update.ts
@@ -0,0 +1,47 @@
+import path from "node:path";
+import { mkdir, readFile } from "node:fs/promises";
+
+import type { Request, Response } from "express";
+
+import { env } from "../../../utils/misc.js";
+import sh from "../../../utils/sh.js";
+
+export const PKG_DIR = path.join(env("DATA_DIR"), "respec", "package");
+
+export default async function route(req: Request, res: Response) {
+  if (req.body.action !== "released") {
+    res.status(400); // Bad request
+    res.locals.reason = `action-not-released`;
+    const msg = `Webhook payload was for ${req.body.action}, ignored.`;
+    return res.send(msg);
+  }
+
+  try {
+    await pullRelease();
+    res.sendStatus(200); // ok
+  } catch (error) {
+    const { message = "", statusCode = 500 } = error;
+    console.error(`Failed to pull respec release: ${message.slice(0, 400)}...`);
+    res.status(statusCode);
+    res.send(message);
+  }
+}
+
+export async function pullRelease() {
+  const start = Date.now();
+  console.log("Pulling latest respec release...");
+
+  const dir = path.resolve(PKG_DIR, "..");
+  await mkdir(dir, { recursive: true });
+  await sh(`npm view respec dist.tarball | xargs curl -s | tar -xz --totals`, {
+    cwd: dir,
+    output: "stream",
+  });
+
+  const { version } = JSON.parse(
+    await readFile(path.join(PKG_DIR, "package.json"), "utf8"),
+  );
+  console.log(
+    `Successfully pulled respec v${version} in ${Date.now() - start}ms.`,
+  );
+}
